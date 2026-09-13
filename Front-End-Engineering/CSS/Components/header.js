dca3f2e25b94de4f1776b1246964cbfb54d8df80
user.email <mohammedsoliman144@gmail.com>
2026-09-13 12:00:31 +0300

    update static header component

--- Front-End-Engineering/CSS/Components/header.js
+++ Front-End-Engineering/CSS/Components/header.js
@@ -1,8 +1,18 @@
 const hamburgerBtn = document.getElementById('hamburgerBtn');
 
 hamburgerBtn.addEventListener("click", function (e) {
-    e.preventDefault()
+    // not need to use e.preventDefault which button type=buttton not submit which means not trigger page reload
+    // e.preventDefault()
     // Toggle if toggled class return true if not return false
     const isOpen =  e.currentTarget.classList.toggle("is-open")
     e.currentTarget.setAttribute("aria-expanded", String(isOpen))
+})
+
+// When user press escape close mobile-menu
+document.addEventListener("keydown", function (e) {
+    if(e.key === 'Escape' && hamburgerBtn.classList.contains("is-open")) {
+        hamburgerBtn.classList.remove("is-open")
+        hamburgerBtn.setAttribute("aria-expanded", "false")
+        hamburgerBtn.focus()
+    }
 })
