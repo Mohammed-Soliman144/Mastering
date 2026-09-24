063e1f96c87764441a134927d13230976539fb89
user.email <mohammedsoliman144@gmail.com>
2026-09-24 11:09:05 +0300

    upload 206. reverse linkedlist - use two pointers technique (curr and prev) with temporary reference points to next node (Optimal) - reverse linkedlist by recursion (second solution O(N): T - O(N):S not optimal)

--- DSA/easy/reverse-linked-list/reverseLinkedList.js
+++ DSA/easy/reverse-linked-list/reverseLinkedList.js
@@ -21,3 +21,24 @@ var reverseLinkedList = function(head) {
     }
     return prev
 }
+
+// Use Recursion to reverse LinkedList (not optimal) O(N): T - O(N): S + Call Stack
+var reverseListByRecursion = function(head) {
+    // BASE CASE
+    if(head === null || head.next === null)
+        return head
+
+    // Recursive Call with smaller solution
+    // make new head points to first node
+    // head => 1 => 2 => 3 => null (before recursion)
+    // null <== 1 => 2 => 3 => null (after first recursive call)
+    //               * new head
+    const newHead = reverseListByRecursion(head.next)
+    // make second node points to head
+    head.next.next = head
+    // make head points to null
+    head.next = null
+
+    // Contract Return newHead
+    return newHead
+}
