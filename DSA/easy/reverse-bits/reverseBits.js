d365c9c1b3ffb4c4542efeb92911bab50af4afeb
user.email <mohammedsoliman144@gmail.com>
2026-09-25 18:46:40 +0300

    upload 190. reverse bits - use right shift and left shift bitwise operator, OR bitwise operator - optimal O(log N): T - O(1): S

---
+++ DSA/easy/reverse-bits/reverseBits.js
@@ -0,0 +1,23 @@
+/**
+* >> signed right shift Bitwise operator
+* >>> unsigned right shift Bitwise operator which means fill first bit on the left side by 0
+* O(1): T - O(1): S
+* note integer contains 32 signed bits => so O(1) constant
+* @param {number} n
+* @return {number} reverse number
+*/
+var reverseBits = function(n) {
+    // declare reverse binary = 0
+    let reverse = 0
+    // problem reveals that each integer number contains 32 signed bits (may be positive or negative) 
+    for(let i =0; i < 32; i++) {
+        // extract last bit in the mostright hand in n
+        let lastBit = n & 1
+        // shift all bits of reverse one step to the left side to empty position of next bit inserted the insert lastBit in this position by OR Bitwise operator
+        reverse = (reverse << 1) | lastBit
+        // shift all bits of n one step to right side to ignored lastBit added to reverse integer
+        n = n >>> 1
+    }
+    // return reverse integer after converted to signed integer by (>>> 0)
+    return reverse >>> 0
+}
